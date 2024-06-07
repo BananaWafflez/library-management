dcb790b5ace8015287221208086ffe3d6e93ffc1
BananaWafflez <benaing12@gmail.com>
2024-06-07 16:27:15 -0400

    initialization

---
+++ script.js
@@ -0,0 +1,21 @@
+const Book = {
+    title: "",
+    author: "",
+    pages: 0
+
+};
+
+
+
+const myLibrary = [];
+
+//book constructor
+function Book(title, author, pages){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+}
+//function
+function addBookToLibrary(){
+
+}
