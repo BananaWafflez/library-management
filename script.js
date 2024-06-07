e0c76fde4b9763e944205e6115320214046110f9
BananaWafflez <benaing12@gmail.com>
2024-06-07 16:42:24 -0400

    Created basic layout for website

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-const Book = {
-    title: "",
-    author: "",
-    pages: 0
 
-};
 
 
 
@@ -16,6 +11,27 @@ function Book(title, author, pages){
     this.pages = pages;
 }
 //function
-function addBookToLibrary(){
-
+function addBookToLibrary(book){
+    myLibrary.push(book);
 }
+
+function displayBooks(){
+    for(let i = 0; i < myLibrary.length; i++){
+        console.log(myLibrary[i].title);
+        console.log(myLibrary[i].author);
+        console.log(myLibrary[i].pages);
+    }
+}
+var book1 = new Book("Harry Potter", "J.K Rowling", 300);
+var book2 = new Book("Learning How to Learn", "Barbara Oakley", 175);
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+displayBooks();
+
+const library = document.getElementById("library");
+var bookcard = document.createElement("div");
+
+
+
+library.appendChild
+
